1b07964d337aacfbaa68e3ef55e035da9979c3aa
poper <poper@wangxutech.com>
2022-05-23 21:15:15 +0800

    init

--- src/utils/request.js
+++ src/utils/request.js
@@ -19,7 +19,7 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers['token'] = getToken()
     }
     return config
   },
@@ -35,7 +35,7 @@ service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
    * Please return  response => response
-  */
+   */
 
   /**
    * Determine the request status by custom code
